f99a43ef35ef45e93361c74e532017a919bb254d
mattjones315 <mattjones315@gmail.com>
2018-12-05 11:38:44 -0800

    setup.py and README.rst

--- ProcessingPipeline/process/sequencing/Visualize/interactive/scatter_case_sensitive.coffee
+++
@@ -1,2 +0,0 @@
-models = cb_obj.document._all_models_by_name._dict
-models['scatter_search_callback'].func(models['search'], '', require, exports)
--- TreeSolver/setup.py
+++
@@ -1,6 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-
-setup(
-        ext_modules=cythonize(["lineage_solver/solver_utils.pyx", "simulation_tools/dataset_generation.pyx"]),
-)
