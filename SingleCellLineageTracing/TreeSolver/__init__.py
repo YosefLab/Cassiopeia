cf79090f785f491898b1f866fe0891e21570f06b
mattjones315 <mattjones315@gmail.com>
2018-12-06 14:45:16 -0800

    removed script import from SingleCellLineageTracing's __init__.py

---
+++ SingleCellLineageTracing/TreeSolver/__init__.py
@@ -0,0 +1,7 @@
+"""Top level for Tree Solver development."""
+
+__author__ = "Matt Jones, Alex Khodaverdian"
+__email__ = "mattjones315@berkeley.edu"
+__version__ = '0.0.1'
+
+from data_pipeline import *
