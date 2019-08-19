f7d35aae411777d3cbbf69adffe7be17c0064ed6
mattjones315 <mattjones315@gmail.com>
2019-08-19 15:51:57 -0700

    added meta.yaml for conda

--- cassiopeia/TreeSolver/__init__.py
+++ cassiopeia/TreeSolver/__init__.py
@@ -1,7 +1,7 @@
 """Top level for Tree Solver development."""
 
-from Cassiopeia.TreeSolver.Node import Node
-from Cassiopeia.TreeSolver.Cassiopeia_Tree import Cassiopeia_Tree
+from cassiopeia.TreeSolver.Node import Node
+from cassiopeia.TreeSolver.Cassiopeia_Tree import Cassiopeia_Tree
 
 __author__ = "Matt Jones, Alex Khodaverdian"
 __email__ = "mattjones315@berkeley.edu"
