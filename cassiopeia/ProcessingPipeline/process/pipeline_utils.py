2f79e55c450122e090eb6233acd33ec9c446d75e
Richard Zhang <richardyz98@s124.Millennium.Berkeley.EDU>
2020-10-02 12:44:38 -0700

    committing changes to pull from process_pipeline_refactor

--- cassiopeia/ProcessingPipeline/process/pipeline_utils.py
+++ cassiopeia/ProcessingPipeline/process/pipeline_utils.py
@@ -27,55 +27,6 @@ HIGH_Q = 31
 LOW_Q = 10
 N_Q = 2
 
-def collapseUMIs(base_dir, fn, max_hq_mismatches = 3, max_indels = 2, max_UMI_distance = 2, n_threads = 1, show_progress=True, force_sort=True):
-    """
-    Collapses UMIs together from a bam file. On a basic level, it aggregates together identical reads to count how many times a UMI was read.
-    Also, it performs basic error correction, allowing UMIs to be collapsed together which differ by at most a certain number of high quality 
-    mismatches and indels in the sequence read itself. Writes out a dataframe of the collapsed UMIs table.
-
-    :param base_dir:
-        Base directory in which to look for a bam file. This will also be where the collapsed matrix is written to.
-    :param fn:
-        File name of the bam file -- just the name, not the entire file path.
-    :param max_hq_mismatches:
-        Maximum number of high quality mismatches allowed between two seqeunces to be collapsed.
-    :param max_indels:
-        Maximum number of indels allowed between two sequences to be collapsed.
-    :param n_threads:
-        Number of threads used. Currently only supports single threaded use.
-    :param show_progress:
-        Allow progress bar to be shown.
-    :param force_sort:
-        Sort the initial bam directory. 
-    :return:
-        None; output table is written to file.
-    """
-
-    base_dir = Path(base_dir)
-    sorted_fn = Path('.'.join(str(base_dir / fn).split(".")[:-1]) + "_sorted.bam")
-
-    sort_key = lambda al: (al.get_tag(CELL_BC_TAG), al.get_tag(UMI_TAG))
-    filter_func = lambda al: al.has_tag(CELL_BC_TAG)
-
-    print(str(sorted_fn))
-    if force_sort or not sorted_fn.exists():
-        sorted_fn = Path('.'.join(fn.split(".")[:-1]) + "_sorted.bam")
-        collapse.sort_cellranger_bam(fn, str(sorted_fn), sort_key, filter_func, show_progress=show_progress)
-
-    collapsed_fn = sorted_fn.with_suffix(".collapsed.bam")
-    print(str(collapsed_fn))
-    if not collapsed_fn.exists():
-        collapse.form_collapsed_clusters(str(sorted_fn),
-                            max_hq_mismatches,
-                            max_indels,
-                            max_UMI_distance,
-                            show_progress=show_progress
-                           )
-    
-    collapsed_df_fn = sorted_fn.with_suffix(".collapsed.txt")
-    collapseBam2DF(str(collapsed_fn), str(collapsed_df_fn)) 
-
-
 def errorCorrectUMIs(input_fn, _id, log_file, max_UMI_distance=2, show_progress=True):
     """
     Error correct UMIs together within equivalence classes, as defined as the same cellBC-intBC pair. UMIs whose identifier 
