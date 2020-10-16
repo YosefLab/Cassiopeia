c3cb525a0f38db6d98a8d5ca2dfdd22f99fd9278
Matthew Jones <mattjones@s124.Millennium.Berkeley.EDU>
2020-10-16 09:52:33 -0700

    added greedy solver

--- cassiopeia/solver/CassiopeiaSolver.py
+++ cassiopeia/solver/CassiopeiaSolver.py
@@ -1,5 +1,5 @@
 """
-Abstract class Cassiopeia-Solver, or the phylogenetic inference module.
+Abstract class CassiopeiaSolver, for the phylogenetic inference module.
 
 All algorithms are derived classes of this abstract class, and at a minimum
 store an input character matrix and implement a method called `solve`. Each
@@ -8,16 +8,29 @@ inferring a phylogenetic tree.
 """
 import abc
 import pandas as pd
+from typing import Dict, Optional
 
 
 class CassiopeiaSolver(abc.ABC):
+    """
+  CassiopeiaSolver is an abstract class that all inference algorithms derive
+  from. At minimum, all CassiopeiaSolver subclasses will store a character
+  matrix and implement a solver procedure.
+  """
 
-  def __init__(self, character_matrix: pd.DataFrame, meta_data: pd.DataFrame):
+    def __init__(
+        self,
+        character_matrix: pd.DataFrame,
+        meta_data: Optional[pd.DataFrame] = None,
+        priors: Optional[Dict] = None,
+    ):
 
-    self.character_matrix = character_matrix
-    self.meta_data = meta_data
-    self.tree = None
+        self.character_matrix = character_matrix
+        self.meta_data = meta_data
+        self.tree = None
 
-  @abc.abstractmethod
-  def solve(self):
-    pass
+    @abc.abstractmethod
+    def solve(self):
+        """Solve the inference problem
+    """
+        pass
