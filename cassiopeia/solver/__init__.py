9398812946eb50d5f1fc745c64cde3f9d0bf28c1
Richard Zhang <richardyz98@s124.Millennium.Berkeley.EDU>
2020-12-18 14:20:51 -0800

    pulling from refactor

--- cassiopeia/solver/__init__.py
+++ cassiopeia/solver/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = "Matt Jones, Alex Khodaverdian"
 __email__ = "mattjones315@berkeley.edu"
-__version__ = '0.0.1'
+__version__ = "0.0.1"
 
 from .NeighborJoiningSolver import NeighborJoiningSolver
