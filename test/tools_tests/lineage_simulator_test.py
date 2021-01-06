1cdd4025b7c7e9cf791844ebb7edd65bfabb0348
Sebastian Prillo <10426884+sprillo@users.noreply.github.com>
2021-01-05 19:05:49 -0800

    more testing

--- test/tools_tests/lineage_simulator_test.py
+++ test/tools_tests/lineage_simulator_test.py
@@ -28,19 +28,32 @@ def test_BirthProcess():
     Generate tree, then choose a random lineage can count how many nodes are on
     the lineage. This is the number of times the process triggered on that
     lineage.
+
+    Also, the probability that a tree with only one internal node is obtained is
+    e^-lam * (1 - e^-lam) where lam is the birth rate, so we also check this.
     """
     np.random.seed(1)
     birth_rate = 0.6
     intensities = []
-    for _ in range(10000):
+    repetitions = 10000
+    topology_hits = 0
+    for _ in range(repetitions):
         tree_true = BirthProcess(
             birth_rate=birth_rate, tree_depth=1.0
         ).simulate_lineage()
+        if len(tree_true.nodes()) == 4:
+            topology_hits += 1
         leaf = np.random.choice(tree_true.leaves())
         n_leaves = len(tree_true.leaves())
         n_hits = tree_true.num_ancestors(leaf) - 1
         intensity = n_leaves / 2 ** n_hits * n_hits
         intensities.append(intensity)
+    # Check that the probability of the topology matches
+    empirical_topology_prob = topology_hits / repetitions
+    theoretical_topology_prob = np.exp(-birth_rate) * (
+        1.0 - np.exp(-birth_rate)
+    )
+    assert np.abs(empirical_topology_prob - theoretical_topology_prob) < 0.02
     inferred_birth_rate = np.array(intensities).mean()
     print(f"{birth_rate} == {inferred_birth_rate}")
     assert np.abs(birth_rate - inferred_birth_rate) < 0.05
