3966479cbecee455c048ff56b26528eec4cb36fa
Sebastian Prillo <10426884+sprillo@users.noreply.github.com>
2021-02-26 14:48:10 -0800

    Docs docs docs

--- cassiopeia/simulator/__init__.py
+++ cassiopeia/simulator/__init__.py
@@ -1,3 +1,3 @@
-from DataSimulator import DataSimulator
-from LineageTracingDataSimulator import LineageTracingDataSimulator
-from TreeSimulator import TreeSimulator
+from .DataSimulator import DataSimulator
+from .LineageTracingDataSimulator import LineageTracingDataSimulator
+from .TreeSimulator import TreeSimulator
