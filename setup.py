e93dc67e1cd049d81dfcac45f993fc5e93ed9f61
Matthew Jones <mattjones@s124.CS.Berkeley.EDU>
2021-03-21 15:32:13 -0700

    upgraded numpy requriement

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md") as readme_file:
 
 
 requirements = [
-        "numpy > 1.17",
+        "numpy > 1.19",
         "matplotlib >= 2.2.2",
         "pandas >= 0.22.0",
         "networkx >= 2.5",
