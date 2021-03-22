b1883e684363c0fe06f2b6997bb3dc3e99529e41
Matthew Jones <mattjones@s124.CS.Berkeley.EDU>
2021-03-22 16:16:36 -0700

    updated readthedocs.yml

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ import warnings
 from pathlib import Path
 
 HERE = Path(__file__).parent
-sys.path[:0] = [str(HERE.parent), str(HERE / "extensions")]
+# sys.path[:0] = [str(HERE.parent), str(HERE / "extensions")]
 
 import cassiopeia # noqa
 
@@ -58,7 +58,8 @@ extensions = [
     "scanpydoc.elegant_typehints",
     "scanpydoc.definition_list_typed_field",
     "scanpydoc.autosummary_generate_imported",
-    *[p.stem for p in (HERE / "extensions").glob("*.py")],
+    *[p.stem for p in (HERE / "extensions").glob("*.py"),
+    *[p.stem for p in (HERE / "extensions").glob("*.pyx")]],
 ]
 
 # nbsphinx specific settings
