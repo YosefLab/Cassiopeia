254841d5fed2dabf2a868bf8237fae44dc1b2aab
Matthew Jones <mattjones@s124.CS.Berkeley.EDU>
2021-03-22 20:28:41 -0700

    importing birthdeathsimulator at simulator init

--- cassiopeia/simulator/__init__.py
+++ cassiopeia/simulator/__init__.py
@@ -2,3 +2,4 @@ from .DataSimulator import DataSimulator
 from .LineageTracingDataSimulator import LineageTracingDataSimulator
 from .TreeSimulator import TreeSimulator
 from .SimpleFitSubcloneSimulator import SimpleFitSubcloneSimulator
+from .BirthDeathFitnessSimulator import BirthDeathFitnessSimulator
