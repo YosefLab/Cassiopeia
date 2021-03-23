1e168193e848f475a3e097d9325b68a043b16b11
Matthew Jones <mattjones@s124.CS.Berkeley.EDU>
2021-03-23 15:41:07 -0700

    added sim.py

--- cassiopeia/simulator/__init__.py
+++ cassiopeia/simulator/__init__.py
@@ -3,6 +3,7 @@
 from .DataSimulator import DataSimulator
 from .LineageTracingDataSimulator import LineageTracingDataSimulator
 from .TreeSimulator import TreeSimulator
+from .BirthDeathFitnessSimulator import BirthDeathFitnessSimulator
 from .SimpleFitSubcloneSimulator import SimpleFitSubcloneSimulator
 from .Cas9LineageTracingDataSimulator import Cas9LineageTracingDataSimulator
 
