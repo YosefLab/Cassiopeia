0ca2157f788d3ea0b9fff4096572973af6acaa2d
Ivan Alexander Kristanto <ivalexander13@s124.CS.Berkeley.EDU>
2021-10-24 21:07:55 -0700

    changed to fit new get method

--- cassiopeia/solver/SpectralNeighborJoiningSolver.py
+++ cassiopeia/solver/SpectralNeighborJoiningSolver.py
@@ -10,15 +10,21 @@ import numpy as np
 import pandas as pd
 from itertools import chain
 
+# Only for debugging purposes
+if True:
+    import sys
+    sys.path.insert(0, '../..')
+
+from cassiopeia.solver.DistanceSolver import DistanceSolver
+
 from cassiopeia.data import CassiopeiaTree
 from cassiopeia.solver import (
-    NeighborJoiningSolver,
     dissimilarity_functions,
     solver_utilities,
 )
 
 
-class SpectralNeighborJoiningSolver(NeighborJoiningSolver):
+class SpectralNeighborJoiningSolver(DistanceSolver):
     """
     Spectral Neighbor-joining class for Cassiopeia.
 
@@ -40,30 +46,81 @@ class SpectralNeighborJoiningSolver(NeighborJoiningSolver):
     ):
         super().__init__(dissimilarity_function, add_root=add_root, prior_transformation=prior_transformation) #type: ignore
 
+    def root_tree(
+        self, tree: nx.Graph, root_sample: str, remaining_samples: List[str]
+    ) -> nx.DiGraph():
+        """Roots a tree produced by Neighbor-Joining at the specified root.
 
-    def find_cherry(self, dissimilarity_matrix: np.ndarray, lambda_indices: List[List[int]] = None) -> Tuple[int, int]:
-        """Finds a pair of samples to join into a cherry.
-
-        Proceeds by minimizing the second largest SVD of the RA Matrix of some subset of nodes.
+        Uses the specified root to root the tree passed in
 
         Args:
-            dissimilarity_matrix: A sample x sample dissimilarity matrix
+            tree: Networkx object representing the tree topology
+            root_sample: Sample to treat as the root
+            remaining_samples: The last two unjoined nodes in the tree
+
+        Returns:
+            A rooted tree
+        """
+        tree.add_edge(remaining_samples[0], remaining_samples[1])
+
+        rooted_tree = nx.DiGraph()
+        for e in nx.dfs_edges(tree, source=root_sample):
+            rooted_tree.add_edge(e[0], e[1])
+
+        return rooted_tree
+
+    def get_dissimilarity_map(
+        self, 
+        cassiopeia_tree: CassiopeiaTree,
+        layer: Optional[str] = None
+    ) -> pd.DataFrame: 
+        """Outputs the first lambda matrix, where every subset is an individual node.
+
+        Args:
+            cassiopeia_tree: the CassiopeiaTree object passed into solve()
+            layer: Layer storing the character matrix for solving. If None, the
+                default character matrix is used in the CassiopeiaTree.
+
+        Returns:
+            DataFrame object of the lamba matrix, but similar in structure to DistanceSolver's dissimilarity_map.
+        """
+
+        # get similarity map and save it as private instance variable
+        self.__similarity_map = super().get_dissimilarity_map(cassiopeia_tree, layer)
+
+        # generate first (pairwise) lambda matrix
+        N = self.__similarity_map.shape[0]
+        self.lambda_indices: List[List[int]] = [[i] for i in range(N)]
+        node_names: np.ndarray = self.__similarity_map.index.values
+
+        lambda_matrix_arr = self.__compute_lambda(self.lambda_indices)
+
+        lambda_matrix_df = pd.DataFrame(
+            lambda_matrix_arr, 
+            index=node_names,
+            columns=node_names
+        )        
+
+        return lambda_matrix_df
+
+
+    def find_cherry(self, dissimilarity_map: np.ndarray) -> Tuple[int, int]:
+        """Finds a pair of samples to join into a cherry.
+
+        With dissimilarity_map being the lambda matrix, this method finds the argmin pair of subsets of the lambda matrix. 
+
+        Args:
+            dissimilarity_matrix: Lambda matrix
 
         Returns:
             A tuple of intgers representing rows in the dissimilarity matrix
                 to join.
         """
-        # temp? to pass tests
-        if not lambda_indices:
-            lambda_indices = [[i] for i in range(dissimilarity_matrix.shape[0])]
 
-        lambda_matrix = self.__compute_lambda(dissimilarity_matrix, lambda_indices)
-        np.fill_diagonal(lambda_matrix, np.inf)
-
-        return np.unravel_index(np.argmin(lambda_matrix, axis=None), lambda_matrix.shape) # TODO: cache with heapq?
+        return np.unravel_index(np.argmin(dissimilarity_map, axis=None), dissimilarity_map.shape) # type: ignore
 
 
-    def __compute_lambda(self, dissimilarity_map: np.ndarray, lambda_indices: List[List[int]]) -> np.ndarray:
+    def __compute_lambda(self, lambda_indices: List[List[int]]) -> np.ndarray:
         """Computes the lambda matrix, whose entries consist of the second SVD of the R^A affinity matrix for every pair of nodes.
 
         Computes the lambda(i,j) = SVD # 2 for R^(Ai U Aj) where i and j are the susbet indices in lambda_indices.
@@ -76,7 +133,7 @@ class SpectralNeighborJoiningSolver(NeighborJoiningSolver):
         """
 
         n = len(lambda_indices)
-        lambda_matrix = np.zeros([n, n])
+        lambda_matrix_arr = np.zeros([n, n])
 
         for i_count, i in enumerate(lambda_indices):
             for j_count, j in enumerate(lambda_indices):
@@ -92,29 +149,43 @@ class SpectralNeighborJoiningSolver(NeighborJoiningSolver):
                 a_comp_flat = list(chain.from_iterable(a_comp))
 
                 # create RA matrix
-                RA_matrix = dissimilarity_map[np.ix_(a_subset, a_comp_flat)]
+                RA_matrix = self.__similarity_map.values[np.ix_(a_subset, a_comp_flat)]
 
                 # Calculate SVD2
-                lambda_matrix[i_count, j_count] = lambda_matrix[j_count, i_count] = np.linalg.svd(RA_matrix)[1][1]
+                lambda_matrix_arr[i_count, j_count] = lambda_matrix_arr[j_count, i_count] = np.linalg.svd(RA_matrix)[1][1]
 
-        return lambda_matrix
+        np.fill_diagonal(lambda_matrix_arr, np.inf)
 
-    def __update_lambda_indices(
+        return lambda_matrix_arr
+
+    def update_dissimilarity_map(
         self,
-        lambda_indices: List[List[int]],
-        node_names: np.ndarray,
+        dissimilarity_map: pd.DataFrame, # lambda matrix
         cherry: Tuple[str, str],
         new_node: str,
-        ) -> Tuple[List[List[int]], np.ndarray]:
+        ) -> pd.DataFrame:
+        """Updates the lambda matrix using the pair of subsets from find_cherry.
 
-        lambda_indices_copy = lambda_indices.copy()
+        Args:
+            dissimilarity_map: lambda matrix to update
+            cherry1: One of the children to join.
+            cherry2: One of the children to join.
+            new_node: New node name to add to the dissimilarity map
 
+        Returns:
+            An updated lambda matrix.
+        """
+
+        lambda_indices_copy = self.lambda_indices.copy()
+
+        # get cherry nodes in index of lambda matrix
         i, j = (
-            np.where(node_names == cherry[0])[0][0], # type: ignore
-            np.where(node_names == cherry[1])[0][0], # type: ignore
+            np.where(dissimilarity_map.index == cherry[0])[0][0],
+            np.where(dissimilarity_map.index == cherry[1])[0][0],
         )
 
         # modify names
+        node_names = dissimilarity_map.index.values
         node_names = np.append(node_names, new_node) # type: ignore
         node_names = np.delete(node_names, [i, j])
 
@@ -123,106 +194,71 @@ class SpectralNeighborJoiningSolver(NeighborJoiningSolver):
         lambda_indices_copy.pop(max(i, j))
         lambda_indices_copy.pop(min(i, j))
 
-        return (lambda_indices_copy, node_names)
+        if len(lambda_indices_copy) > 2:
+            # compute new lambda matrix
+            lambda_matrix_arr = self.__compute_lambda(lambda_indices_copy)
+        else:
+            n = len(lambda_indices_copy)
+            lambda_matrix_arr = np.zeros((n, n))
 
-
-    # TODO: document the change: just replace update dissimilarity map into update lambda_index matrix
-    def solve(
-        self,
-        cassiopeia_tree: CassiopeiaTree,
-        layer: Optional[str] = None,
-        collapse_mutationless_edges: bool = False,
-        logfile: str = "stdout.log",
-    ) -> None:
-        """Solves a tree for a general bottom-up distance-based solver routine.
-
-        The general solver routine proceeds by iteratively finding pairs of
-        samples to join together into a "cherry" and then reform the
-        dissimilarity matrix with respect to this new cherry. The implementation
-        of how to find cherries and update the dissimilarity map is left to
-        subclasses of DistanceSolver. The function will update the `tree`
-        attribute of the input CassiopeiaTree.
-
-        Args:
-            cassiopeia_tree: CassiopeiaTree object to be populated
-            layer: Layer storing the character matrix for solving. If None, the
-                default character matrix is used in the CassiopeiaTree.
-            collapse_mutationless_edges: Indicates if the final reconstructed
-                tree should collapse mutationless edges based on internal states
-                inferred by Camin-Sokal parsimony. In scoring accuracy, this
-                removes artifacts caused by arbitrarily resolving polytomies.
-            logfile: File location to log output. Not currently used.
-        """
-        node_name_generator = solver_utilities.node_name_generator()
-
-        self.setup_dissimilarity_map(cassiopeia_tree, layer)
-
-        dissimilarity_map = cassiopeia_tree.get_dissimilarity_map()
-
-        N = dissimilarity_map.shape[0]
-
-        # instantiate a dissimilarity map that can be updated as we join
-        # together nodes.
-        _dissimilarity_map = dissimilarity_map.copy()
-
-        # instantiate a tree where all samples appear as leaves.
-        tree = nx.Graph()
-        tree.add_nodes_from(_dissimilarity_map.index)
-
-        # Construct Lambda Indices matrix
-        lambda_indices: List[List[int]] = [[i] for i in range(N)]
-        node_names: np.ndarray = _dissimilarity_map.index.values
-
-        while N > 2:
-            print(f'# Running with N={N}')
-            i, j = self.find_cherry(_dissimilarity_map.to_numpy(), lambda_indices)
-
-            # get node names to join
-            node_i, node_j = (
-                node_names[i],
-                node_names[j],
-            ) 
-
-            new_node_name = next(node_name_generator)
-            tree.add_node(new_node_name)
-            tree.add_edges_from([
-                (new_node_name, node_i), 
-                (new_node_name, node_j)
-            ])
-
-            # Changed for SNJ
-            lambda_indices, node_names = self.__update_lambda_indices(
-                    lambda_indices, 
-                    node_names,
-                    (node_i, node_j), 
-                    new_node_name
-                )
-
-            N = len(lambda_indices)
-
-        tree = self.root_tree(
-            tree,
-            cassiopeia_tree.root_sample_name,
-            node_names,
+        # regenerate lambda matrix
+        lambda_matrix_df = pd.DataFrame(
+            lambda_matrix_arr,
+            index=node_names,
+            columns=node_names
         )
 
-        # remove root from character matrix before populating tree
-        if (
-            cassiopeia_tree.root_sample_name
-            in cassiopeia_tree.character_matrix.index
-        ):
-            cassiopeia_tree.character_matrix = (
-                cassiopeia_tree.character_matrix.drop(
-                    index=cassiopeia_tree.root_sample_name
+        # update lambda indices
+        self.lambda_indices = lambda_indices_copy
+        
+
+        return lambda_matrix_df
+
+    def setup_root_finder(self, cassiopeia_tree: CassiopeiaTree) -> None:
+        """Defines the implicit rooting strategy for the NeighborJoiningSolver.
+
+        By default, the NeighborJoining algorithm returns an unrooted tree.
+        To root this tree, an implicit root of all zeros is added to the
+        character matrix. Then, the dissimilarity map is recalculated using
+        the updated character matrix. If the tree already has a computed
+        dissimilarity map, only the new dissimilarities are calculated.
+
+        Args:
+            cassiopeia_tree: Input CassiopeiaTree to `solve`
+        """
+        character_matrix = cassiopeia_tree.character_matrix.copy()
+        rooted_character_matrix = character_matrix.copy()
+
+        root = [0] * rooted_character_matrix.shape[1]
+        rooted_character_matrix.loc["root"] = root
+        cassiopeia_tree.root_sample_name = "root"
+        cassiopeia_tree.character_matrix = rooted_character_matrix
+
+        if self.dissimilarity_function is None:
+            raise DistanceSolver.DistanceSolverError(
+                "Please specify a dissimilarity function to add an implicit "
+                "root, or specify an explicit root"
+            )
+
+        dissimilarity_map = cassiopeia_tree.get_dissimilarity_map()
+        if dissimilarity_map is None:
+            cassiopeia_tree.compute_dissimilarity_map(
+                self.dissimilarity_function, self.prior_transformation
+            )
+        else:
+            dissimilarity = {"root": 0}
+            for leaf in character_matrix.index:
+                weights = None
+                if cassiopeia_tree.priors:
+                    weights = solver_utilities.transform_priors(
+                        cassiopeia_tree.priors, self.prior_transformation
+                    )
+                dissimilarity[leaf] = self.dissimilarity_function(
+                    rooted_character_matrix.loc["root"].values,
+                    rooted_character_matrix.loc[leaf].values,
+                    cassiopeia_tree.missing_state_indicator,
+                    weights,
                 )
-            )
+            cassiopeia_tree.set_dissimilarity("root", dissimilarity)
 
-        cassiopeia_tree.populate_tree(tree, layer=layer)
-        cassiopeia_tree.collapse_unifurcations()
-
-        # collapse mutationless edges
-        if collapse_mutationless_edges:
-            cassiopeia_tree.collapse_mutationless_edges(
-                infer_ancestral_characters=True
-            )
-
+        cassiopeia_tree.character_matrix = character_matrix
