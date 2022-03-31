92fb43cbf144ae6ec8a751cd7c01e611d27e5a6c
Matthew Jones <mattjones@s124.CS.Berkeley.EDU>
2022-03-30 17:33:43 -0700

    fixed bug in itol imports

--- cassiopeia/plotting/itol_utilities.py
+++ cassiopeia/plotting/itol_utilities.py
@@ -169,7 +169,7 @@ def upload_and_export_itol(
 
         if pd.api.types.is_string_dtype(values):
             colors = palette[: len(values.unique())]
-            colors = [hex_to_rgb(color) for color in colors]
+            colors = [utilities.hex_to_rgb(color) for color in colors]
             colormap = dict(zip(np.unique(values), colors))
 
             files.append(
