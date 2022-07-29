ee40933bb0b70d4cd01ca513abea9704b24aa1b3
Sebastian Prillo <10426884+sprillo@users.noreply.github.com>
2022-07-29 14:28:27 -0700

    Add BranchLengthEstimator to branch_length_estimator package

--- cassiopeia/tools/branch_length_estimator/__init__.py
+++ cassiopeia/tools/branch_length_estimator/__init__.py
@@ -1,4 +1,5 @@
 """Top level for branch length estimator."""
 
+from .BranchLengthEstimator import BranchLengthEstimator
 from .IIDExponentialMLE import IIDExponentialMLE
 from .IIDExponentialBayesian import IIDExponentialBayesian
