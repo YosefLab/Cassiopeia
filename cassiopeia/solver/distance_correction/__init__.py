89980abc8a1c1b6655fe4d85041c776a8ac08579
Sebastian Prillo <10426884+sprillo@users.noreply.github.com>
2023-08-12 15:16:45 -0700

    Dont export

--- cassiopeia/solver/distance_correction/__init__.py
+++ cassiopeia/solver/distance_correction/__init__.py
@@ -2,7 +2,5 @@
 Subpackage for distance correction solvers.
 """
 from ._crispr_cas9_distance_correction_solver import (
-    crispr_cas9_default_collision_probability_estimator,
-    crispr_cas9_default_mutation_proportion_estimator,
     CRISPRCas9DistanceCorrectionSolver
 )
