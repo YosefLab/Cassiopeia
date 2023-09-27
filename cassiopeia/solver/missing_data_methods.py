1097521911818e3bd6fdf1e20d367527a7a8c314
Matthew Jones <mattjones315@gmail.com>
2023-09-27 12:55:40 -0700

    updated missing data classification when ambig states are present

--- cassiopeia/solver/missing_data_methods.py
+++ cassiopeia/solver/missing_data_methods.py
@@ -4,7 +4,7 @@ from typing import Dict, List, Optional, Tuple, Union
 import numpy as np
 import pandas as pd
 
-from cassiopeia.mixins import unravel_ambiguous_states
+from cassiopeia.mixins import is_ambiguous_state, unravel_ambiguous_states
 from cassiopeia.solver import solver_utilities
 
 
@@ -61,18 +61,32 @@ def assign_missing_average(
         for char in range(character_matrix.shape[1]):
             state = character_array[missing_sample, char]
             if state != missing_state_indicator and state != 0:
-                all_states = unravel_ambiguous_states(
-                    subset_character_matrix[:, char]
+                all_states = (
+                    unravel_ambiguous_states(subset_character_matrix[:, char])
                 )
                 state_counts = np.unique(all_states, return_counts=True)
-                ind = np.where(state_counts[0] == state)
-                if len(ind[0]) > 0:
-                    if weights:
-                        score += (
-                            weights[char][state] * state_counts[1][ind[0][0]]
-                        )
-                    else:
-                        score += state_counts[1][ind[0][0]]
+
+                if is_ambiguous_state(state):
+                    for ambig_state in state: 
+                        ind = np.where(state_counts[0] == ambig_state)
+                        if len(ind[0]) > 0:
+                            if weights:
+                                score += (
+                                    weights[char][ambig_state] * state_counts[1][ind[0][0]]
+                                )
+                            else:
+                                score += state_counts[1][ind[0][0]]
+
+                else:
+                    ind = np.where(state_counts[0] == state)
+                    if len(ind[0]) > 0:
+                        if weights:
+                            score += (
+                                weights[char][state] * state_counts[1][ind[0][0]]
+                            )
+                        else:
+                            score += state_counts[1][ind[0][0]]
+                            
         return score
 
     subset_character_array_left = character_array[left_indices, :]
