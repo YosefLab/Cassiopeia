9e40941c53d591982071c8c066ce714a01db3fbb
Matt Jones <mattjones315@gmail.com>
2025-07-22 15:36:20 -0700

    updated docs

--- cassiopeia/spatial/__init__.py
+++ cassiopeia/spatial/__init__.py
@@ -3,8 +3,10 @@
 """Top level for spatial module."""
 
 from .spatial_imputation import impute_spatial_data
+from .spatial_utilities import get_spatial_graph_from_anndata
 
 
 __all__ = [
+    "get_spatial_graph_from_anndata",
     "impute_spatial_data",
 ]
